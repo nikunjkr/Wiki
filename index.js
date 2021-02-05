d8fa7c9e22b44243fe80df82f2dbad38c274b0f6
nikunjkr <nikunjkr078@gmail.com>
2021-02-06 02:31:56 +0530

    submit

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ var timer = setInterval(function () {
 
 // Base URL of wiki
 var base_url = "https://en.wikipedia.org/wiki/"
-// API
 
+// API
 var url = "https://en.wikipedia.org/w/api.php"; 
-// parameters to query
+
+// search_term is the first argument passed 
 var search_term = process.argv[2];
+// parameters to query
 var params = {
     action: "query",
     list: "search",
@@ -38,7 +40,7 @@ fetch(url)
         if (response.query.search[0].title === search_term){
           console.log("Link Fetched! Writing in Progress");
           
-          fs.writeFile(process.argv[3],base_url+search_term.replace(" ","_") ,function (err) {
+          fs.appendFile(process.argv[3],base_url+search_term.replace(" ","_")+"\n" ,function (err) {
             if (err) return console.log(err);
             
           });
